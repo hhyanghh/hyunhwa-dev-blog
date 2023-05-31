2f7d8465b74455da7ef20f7b9219315cb3e2525d
hhyanghh <yanghh711@naver.com>
2023-05-31 19:39:39 +0900

    구조설정

---
+++ data/categories.ts
@@ -0,0 +1,6 @@
+export const categorys: { title: string; keyword: string }[] = [
+  { title: "🐱 All", keyword: "" },
+  { title: "📖 JavaScript", keyword: "javascript" },
+  { title: "🧑🏻‍💻 회고", keyword: "retrospection" },
+  { title: " React", keyword: "react" },
+];
