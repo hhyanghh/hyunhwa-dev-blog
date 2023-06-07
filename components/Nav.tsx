01afe70463f1c5c3fb96bac71b36cefb944d9d89
hhyanghh <yanghh711@naver.com>
2023-06-07 19:29:17 +0900

    Nav active 상태 추가

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,14 +2,27 @@ import React from "react";
 import navlinks from "@/data/nav";
 import Link from "next/link";
 import { CgSun } from "react-icons/cg";
+import { useRouter } from "next/router";
 
 const Nav = () => {
+  const router = useRouter();
+
+  console.log(router.pathname, "router.pathname");
+
   return (
     <nav className="text-navy max-w-screen-md h-20 flex flex-nowrap items-center justify-between m-auto px-8">
       <Link href="/">Home</Link>
       <div className="flex items-center">
         {navlinks.map((nav) => (
-          <Link href={nav.link} key={nav.title} className="mr-5">
+          <Link
+            href={nav.link}
+            key={nav.title}
+            className={`mr-5 ${
+              router.pathname === nav.link
+                ? "font-bold underline underline-offset-4"
+                : ""
+            }`}
+          >
             {nav.title}
           </Link>
         ))}
