40b6c4c962ffa6cf1c49bcd8eedd9c83bdc13757
hhyanghh <yanghh711@naver.com>
2023-06-14 21:51:30 +0900

    blog 페이지 flex 속성

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,9 +2,9 @@ import BlogPost from "@/components/BlogPost";
 
 export default function Blog() {
   return (
-    <>
+    <div className="flex flex-col">
       <div className="font-bold">BLOG</div>
       <BlogPost />
-    </>
+    </div>
   );
 }
