cd1268c5cfe7fcf485ae5f9a87e4e65fed8eec77
hhyanghh <yanghh711@naver.com>
2023-06-16 16:48:15 +0900

    add error page

---
+++ pages/_error.tsx
@@ -0,0 +1,11 @@
+export default function Error() {
+  return (
+    <section className="h-100vh my-10 flex flex-col items-center justify-center h-screen">
+      <h1 className="font-bold text-2xl sm:text-4xl">🙏🏻 Error 🙏🏻</h1>
+      <br />
+      <p className="font-bold text-base sm:text-lg">
+        존재하지 않는 페이지 입니다.
+      </p>
+    </section>
+  );
+}
