2f6a4beb8ed4ae42b95f1839d16b356ad2f230c8
hhyanghh <yanghh711@naver.com>
2023-06-18 19:56:29 +0900

    Style: 메인 프로필영역 작업완료

--- components/HomeProfile.tsx
+++ components/HomeProfile.tsx
@@ -1,22 +1,22 @@
 import Image from "next/image";
 import React from "react";
 import profileImage from "../public/profile.png";
+import bgImage from "../public/bg_sea.JPG";
 
 export default function HomeProfile() {
   return (
-    <div className="mt-10">
-      <section className="flex justify-center gap-8 items-center flex-wrap">
+    <div className="h-80 relative mt-10">
+      <div className="absolute inset-0 overflow-hidden rounded-lg">
         <Image
-          src={profileImage}
-          alt="프로필 이미지"
-          className="rounded-lg"
-          width={300}
+          src={bgImage}
+          alt="Background Image"
+          className="w-full h-full object-cover transform scale-105 transition-transform duration-300 hover:scale-100 hover:brightness-90"
         />
-        <div className="min-w-[250] max-w-[300px]">
-          태양은 만인의 것, <br />
-          바다는 즐기는 자의 것
-        </div>
-      </section>
+      </div>
+      <div className="text-white absolute top-20 left-10 font-semibold text-xl">
+        태양은 만인의 것 <br />
+        바다는 즐기는 자의 것
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        sea: "url('../public/bg_sea.JPG')",
       },
       colors: {
         navy: "#025464",
