d63a559fe6edce6218b61571d7b87f489f4a83d8
hhyanghh <yanghh711@naver.com>
2023-06-18 13:28:30 +0900

    style: 프로필영역 작업 중

--- components/HomeProfile.tsx
+++ components/HomeProfile.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
 import React from "react";
+import profileImage from "../public/profile.png";
 
 export default function HomeProfile() {
   return (
     <div className="mt-10">
-      <h1 className="text-3xl font-extrabold">Home Profile</h1>
-      <div className=" bg-slate-600 h-64 mt-5">HomeProfile</div>
+      <section className="flex justify-center gap-8 items-center flex-wrap">
+        <Image
+          src={profileImage}
+          alt="프로필 이미지"
+          className="rounded-lg"
+          width={300}
+        />
+        <div className="min-w-[250] max-w-[300px]">
+          태양은 만인의 것, <br />
+          바다는 즐기는 자의 것
+        </div>
+      </section>
     </div>
   );
 }
