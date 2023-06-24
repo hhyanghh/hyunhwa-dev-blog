734bd544d18f88e69fb233375e2bf66ecb5931d1
hhyanghh <yanghh711@naver.com>
2023-06-24 11:48:31 +0900

    Add: Footer 컴포넌트 생성 및 위치지정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="absolute left-0 bottom-3 w-full flex justify-center">
+      <div className="flex gap-3 justify-center">
+        <div>Copyright © 2023 YangHyunHwa</div>
+        <div>깃허브(링크)</div>
+        <div>이메일 보내기</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
