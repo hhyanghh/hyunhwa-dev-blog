58736abf3096d3dc0cc9da8abea4e42427a31d05
hhyanghh <yanghh711@naver.com>
2023-06-24 19:30:07 +0900

    Styles: Footer 스타일링

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Footer = () => {
   return (
-    <div className="absolute left-0 bottom-3 w-full flex justify-center">
-      <div className="flex gap-3 justify-center">
-        <div>Copyright © 2023 YangHyunHwa</div>
-        <div>깃허브(링크)</div>
-        <div>이메일 보내기</div>
+    <div className="absolute left-0 bottom-3 w-full">
+      <div className="flex gap-3 justify-center flex-col items-center pt-4">
+        <div className="text-sm text-gray-600 dark:text-white font-md">
+          Copyright © 2023 YangHyunHwa
+        </div>
+        <div className="flex gap-3">
+          <Link href="https://github.com/hhyanghh" target="_blank">
+            <FaGithub size="24" className="hover:text-orange" />
+          </Link>
+          <a href="mailto:yanghh711@gmail.com">
+            <MdEmail size="24" className="hover:text-orange" />
+          </a>
+        </div>
       </div>
     </div>
   );
