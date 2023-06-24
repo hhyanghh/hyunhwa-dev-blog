07cf1deab378bcab27c343fa34e132fa69d0dd07
hhyanghh <yanghh711@naver.com>
2023-06-24 16:58:16 +0900

    Styles: darkmode empty component 수정

--- components/Empty.tsx
+++ components/Empty.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Empty = () => {
   return (
-    <div className="mt-10 text-3xl font-semibold h-80 bg-blue-50 flex justify-center items-center rounded-lg">
+    <div className="mt-10 text-3xl font-semibold h-80 bg-blue-50 flex justify-center items-center rounded-lg dark:text-orange">
       🏄🏻‍♀️ Coming Sooooooon! 🏄🏻‍♀️
     </div>
   );
