caf123aebb3a805886a5220a2ed45e30b5d30e9d
hhyanghh <yanghh711@naver.com>
2023-06-24 17:32:34 +0900

    Styles: error page 스타일링

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,11 +1,29 @@
-export default function Error() {
+import Link from "next/link";
+
+const Error = () => {
+  const navHeight = 160;
   return (
-    <section className=" my-10 flex flex-col items-center justify-center h-80">
-      <h1 className="font-bold text-2xl sm:text-4xl">🙏🏻 Error 🙏🏻</h1>
-      <br />
-      <p className="font-bold text-base sm:text-lg">
-        존재하지 않는 페이지 입니다.
+    <section
+      className="flex flex-col items-center justify-center"
+      style={{ height: `calc(100vh - ${navHeight}px)` }}
+    >
+      <p className="text-base font-semibold text-orange">404</p>
+      <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl dark:text-white">
+        🙏 Page not found 🙏
+      </h1>
+      <p className="mt-6 text-base leading-7 text-gray-600 dark:text-white">
+        Sorry, we couldn’t find the page you’re looking for.
       </p>
+      <div className="mt-10 flex items-center justify-center gap-x-6">
+        <Link
+          href="/"
+          className="rounded-md bg-orange px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Go back home
+        </Link>
+      </div>
     </section>
   );
-}
+};
+
+export default Error;
