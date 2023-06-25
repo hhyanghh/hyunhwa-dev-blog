7e2ace2909d7513060c1f5a3ef4f5be314822afb
hhyanghh <yanghh711@naver.com>
2023-06-25 22:26:28 +0900

    Fix: tailwindcss 에러수정

--- next.config.js
+++ next.config.js
@@ -4,17 +4,6 @@ const { withContentlayer } = require("next-contentlayer");
 const options = {
   reactStrictMode: false,
   swcMinify: false,
-  experimental: {
-    scrollRestoration: true,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.contentlayer$/,
-      use: "raw-loader",
-    });
-
-    return config;
-  },
 };
 
 module.exports = withContentlayer(options);
