c53d970f017b401a37953e45bc8b9e5c09659464
hhyanghh <yanghh711@naver.com>
2023-06-25 12:25:12 +0900

    Fix: Image 경로 정적으로 수정

--- components/HomeProfile.tsx
+++ components/HomeProfile.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import React from "react";
-import profileImage from "../public/profile.png";
-import bgImage from "../public/bg_sea.JPG";
 
 export default function HomeProfile() {
   return (
     <div className="h-80 relative mt-10">
       <div className="absolute inset-0 overflow-hidden rounded-lg">
         <Image
-          src={bgImage}
+          src="/bg_sea.JPG"
           alt="Background Image"
+          width="500"
+          height="100"
           className="w-full h-full object-cover transform transition-transform duration-300 hover:scale-105 hover:brightness-90"
         />
       </div>
